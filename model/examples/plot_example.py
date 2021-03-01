5142f85dea39c8438b79873a4d8e3b97b3f6b41a
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2021-03-01 22:36:16 +0100

    model: simplified activity/cdf example plot

--- model/examples/plot_example.py
+++ model/examples/plot_example.py
@@ -7,64 +7,35 @@ from neslab.find import Model
 prop_cycle = plt.rcParams["axes.prop_cycle"]
 colors = prop_cycle.by_key()["color"]
 
+t_chr = 100
+distribution = "Uniform"
+scales = [30, 60]
 
-def plot_activity(
-    t_chr: int = 100,
-    distribution: str = "Uniform",
-    offset_rel: float = 0.5,
-    n: int = 100000,
-):
-    for i, shape in enumerate([30, 60]):
-        m = Model(shape, distribution, t_chr, n_slots=n)
+cdfs = list()
+for i, scale in enumerate(scales):
 
-        activities = m.activity()
+    m = Model(scale, "Uniform", t_chr)
 
-        dist_cls = getattr(dists, distribution.capitalize())
-        offset = int(dist_cls(shape).expectation() + t_chr / 2)
+    activities = m.activity()
+    cdf = m.cdf()
 
-        act_0 = np.pad(activities[:, 0], 10, mode="constant")[:-offset]
-        act_1 = np.pad(activities[:, 0], 10 + offset, mode="constant")
-
-        f_act, ax_act = plt.subplots(figsize=(5, 1.75))
-        ax_act.plot(act_0, label=r"Node 1", color=colors[0], linewidth=0.75)
-        ax_act.plot(
-            act_1, label=r"Node 2", color=colors[1], linewidth=0.75, linestyle="--"
-        )
-        ax_act.set_xlabel("Slot")
-        ax_act.set_ylabel("Probability\nto be active", multialignment="center")
-        ax_act.set_ylim([-0.0025, 0.041])
-        ax_act.set_xlim((-100, 3990))
-        plt.legend(loc="upper right")
-        plt.tight_layout()
-        plt.show()
-
-
-def plot_cdf(
-    t_chr: int = 100,
-    distribution: str = "Uniform",
-    offset_rel: float = 0.5,
-    n: int = 200000,
-):
-    f_cdf, ax_cdf = plt.subplots(figsize=(5, 2))
-    shape_labels = [30, 60]
-    linestyles = ["-", "--"]
-    for i, shape in enumerate([15, 90]):
-        m = Model(shape, distribution, t_chr, n_slots=n)
-        ax_cdf.semilogx(
-            m.cdf(),
-            label=fr"Random delay $X\sim U[0, {int(shape_labels[i])}]$",
-            linestyle=linestyles[i],
-        )
-
-    ax_cdf.set_xlabel("Slot")
-    ax_cdf.set_ylabel("Cumulative prob.\nof discovery", multialignment="center")
-    ax_cdf.set_xlim((9, ax_cdf.get_xlim()[1]))
-    ax_cdf.set_ylim((-0.05, 1.05))
-    plt.legend(loc="upper left")
-    f_cdf.tight_layout()
+    plt.plot(activities[:4000, 0], label=r"Node 1")
+    plt.plot(activities[:4000, 1], label=r"Node 2")
+    plt.xlabel("Slot")
+    plt.ylabel("Probability of Activity")
+    plt.legend()
+    plt.title(fr"Random delay $X\sim U[0, {int(scale)}]$")
     plt.show()
 
+    cdfs.append(cdf)
 
-if __name__ == "__main__":
-    plot_activity()
-    plot_cdf()
+for i, scale in enumerate(scales):
+    plt.semilogx(
+        cdfs[i],
+        label=fr"Random delay $X\sim U[0, {int(scale)}]$",
+    )
+
+plt.xlabel("Slot")
+plt.ylabel("Cumulative probability of discovery")
+plt.legend(loc="upper left")
+plt.show()
