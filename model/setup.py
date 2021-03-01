38f6a06236a3080faa917dec537553d358a0423a
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2021-03-01 22:51:11 +0100

    bumpversion v0.0.3 --> v0.0.4

--- model/setup.py
+++ model/setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="neslab-find",
-    version="0.0.2",
+    version="0.0.4",
     description="FIND model",
     long_description=README,
     long_description_content_type="text/markdown",
